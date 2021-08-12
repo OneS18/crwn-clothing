b68aa9fdf158b5c71fb1bc946a3a902f20ea29d4
OneS18 <diablo201888@gmail.com>
2021-08-12 11:00:03 +0300

    adding header components and links to homme,shop page...

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import HomePage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
